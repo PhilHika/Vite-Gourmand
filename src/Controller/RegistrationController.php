c6218310e328897f38aede64ea6138383b6d9a8d
Philippe Lavocat <philippe.lavocat@live.fr>
2026-02-06 18:09:34 +0100

    registration : maintenant en s'enregistrant on reste loggé et on est rediriger vers l'acceuil

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -6,6 +6,7 @@ use App\Entity\Utilisateur;
 use App\Form\RegistrationFormType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
@@ -17,7 +18,8 @@ class RegistrationController extends AbstractController
     public function register(
         Request $request,
         UserPasswordHasherInterface $userPasswordHasher,
-        EntityManagerInterface $entityManager
+        EntityManagerInterface $entityManager,
+        Security $security
     ): Response {
         $user = new Utilisateur();
         $form = $this->createForm(RegistrationFormType::class, $user);
@@ -38,9 +40,13 @@ class RegistrationController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
 
+            // Connexion automatique de l'utilisateur après inscription
+            $security->login($user, 'form_login', 'main');
+
             $this->addFlash('success', 'Votre compte a été créé avec succès !');
 
-            return $this->redirectToRoute('app_login');
+            // Redirection vers la page d'accueil au lieu de la page de login
+            return $this->redirectToRoute('app_home');
         }
 
         return $this->render('registration/register.html.twig', [
